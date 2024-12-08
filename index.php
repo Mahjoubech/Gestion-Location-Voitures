265f995ccf256b61c1e96549806a75343512ef9a
Mahjoubech <charkaouielmahjoub50@gmail.com>
2024-12-08 03:48:56 +0100

    connect sql with php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+
+<?php
+ $host = 'localhost';     
+ $dbname = 'location_voiture';  
+ $username = 'root';     
+ $password = 'Mahjoub@123';          
+ 
+ try {
+     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     echo "kbhadaaaaaaaaaaaaaam : ";
+ } catch (PDOException $e) {
+     echo "Erreur de connexion : " . $e->getMessage();
+ }
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +22,7 @@
     <script src="/tailwind.js"></script>
     <title></title>
 </head>
-<body  class=" ">
+<body  class=" bg-slate-600">
     <div>
        hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
     </div>
