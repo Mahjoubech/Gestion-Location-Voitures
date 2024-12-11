004bdd5b674e7dd579493a917249070463c9de86
Mahjoubech <charkaouielmahjoub50@gmail.com>
2024-12-11 23:56:51 +0100

    fix problem connex data base

---
+++ views/databasecnx.php
@@ -0,0 +1,10 @@
+<?php  
+$cnx = mysqli_connect("localhost", "root", "Mahjoub@123", "location_voiture");
+if (!$cnx) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    // echo "Connection successful!";
+}
+
+?>
+
