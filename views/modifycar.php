b34ac9bf44981575c61a75ff55c738f0f2a38eae
Mahjoubech <charkaouielmahjoub50@gmail.com>
2024-12-13 15:53:00 +0100

    modify cars function php

--- views/modifycar.php
+++ views/modifycar.php
@@ -6,9 +6,10 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['editveh'])){
     $mark = $_POST['Mark'];
     $modl = $_POST['Model'];
     $year = $_POST['vehYear'];
-    $updt = "UPDATE `voiture` SET NumImmatriculation= '$numCar', Marque='$mark', Modele='$modl', Annee='$year' WHERE NumImmatriculation=$numCar";
+    $updt = "UPDATE `voiture` SET NumImmatriculation= '$numCar', Marque='$mark', Modele='$modl', Annee='$year' WHERE NumImmatriculation='$numCar'";
      $reslt = mysqli_query($cnx,$updt);
     
 header('Location: ../cars.php');
 }
+
 ?>
